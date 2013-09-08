7d472105b63a990c6c62e3c43efabb44dd43058c
PixelToast <andre.lipke@gmail.com>
2013-09-08 03:06:09 -0400

    cleanup

--- src/conf.lua
+++ src/conf.lua
@@ -1,13 +1,12 @@
-debug = false
+debugmode = false
 function love.conf(t)
     t.title = "ComputerCraft Emulator"
     t.author = "Sorroko"
-    --t.url = nil
     t.version = "0.8.0"
 
     t.modules.physics = false
 
-    if debug then
+    if debugmode then
    	    t.console = true
         t.release = false
 	else
