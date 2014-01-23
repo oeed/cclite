7d9e4721b20fbeb5bf46dedc64b7303145c89f13
Robert Gadzerfraud <gamax92@aol.com>
2014-01-23 15:07:07 -0700

    Tweak scripts

--- build_src.bat
+++ build_src.bat
@@ -1,6 +1,11 @@
 @echo off
-set /P version=Enter version (Format: x.y.z): 
-cd src\
-7z a -r -tzip -mx5 "..\cclite-beta-%version%.love" ".\*"
-cd ..\
+if not exist cclite-latest-beta.love goto makeArchive
+set /P replace=Overwrite cclite-latest-beta.love [Y/N]: 
+if /I NOT %replace%==Y goto end
+del cclite-latest-beta.love
+:makeArchive
+cd src
+..\7za.exe a -r -tzip -mx5 ..\cclite-latest-beta.love ".\*"
+cd ..
+:end
 pause
